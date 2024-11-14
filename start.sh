b0065677b3d05a47f69b8521805df41e397e1090
cybercinogen <lobu83898@gmail.com>
2024-11-14 18:46:39 +0530

    Add templates and static folders

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Start the scheduler in the background
-echo "Starting scheduler..."
-python scheduler.py &
-
-# Start the Flask app using Gunicorn
-echo "Starting Flask app with Gunicorn..."
-gunicorn -b 0.0.0.0:$PORT app:app
+# Start the scheduler script
+python scheduler.py
